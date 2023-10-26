d20214a964c8f04b5d46df4c995d469304d6d8a0
Davi Rezende <rezendemota.davi@gmail.com>
2023-10-25 23:22:39 -0300

    fzf keymappings

--- lua/davi/plugins/init.lua
+++ lua/davi/plugins/init.lua
@@ -1,9 +1,9 @@
 return {
-   'tpope/vim-surround',
-   'tpope/vim-commentary',
-   'vim-airline/vim-airline',
-   'ap/vim-css-color',
-   'terryma/vim-multiple-cursors',
-   'preservim/tagbar',
-   'kshenoy/vim-signature',
+  'tpope/vim-surround',
+  'tpope/vim-commentary',
+  'vim-airline/vim-airline',
+  'ap/vim-css-color',
+  'terryma/vim-multiple-cursors',
+  'preservim/tagbar',
+  'kshenoy/vim-signature',
 }
--- lua/davi/plugins/telescope.lua
+++ lua/davi/plugins/telescope.lua
@@ -6,10 +6,11 @@ return {
   },
 
   config = function()
+
     local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-    vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-    vim.keymap.set('n', '<leader>ps', function()
+    vim.keymap.set('n', 'ff', builtin.find_files, {})
+    vim.keymap.set('n', 'fg', builtin.git_files, {})
+    vim.keymap.set('n', 'fs', function()
       builtin.grep_string({ search = vim.fn.input("Grep > ") })
     end)
   end
